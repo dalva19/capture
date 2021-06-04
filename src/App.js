5d7888e480e63fd2f86124af2fd5dd8a666918a0
dalva19 <75229602+dalva19@users.noreply.github.com>
2021-06-04 14:57:09 -0400

    about section

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react'; 
+//import pages
+import AboutUs from './pages/AboutUs';
 
 function App() {
   return (
     <div className="App">
-     <h1>Hello</h1>
+     <AboutUs /> 
     </div>
   );
-}
+};
 
 export default App;
