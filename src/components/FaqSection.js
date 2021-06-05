a124b343cd603dd5876766b5d37f771ea271fda1
dalva19 <75229602+dalva19@users.noreply.github.com>
2021-06-05 15:01:27 -0400

    styled faq section

--- src/components/FaqSection.js
+++ src/components/FaqSection.js
@@ -1,8 +1,12 @@
 import React from "react";
 
+// styles
+import { About } from "../styles";
+import styled from "styled-components";
+
 const FaqSection = () => {
   return (
-    <div className="faq">
+    <Faq>
       <h2>
         Any Questions? <span>FAQ</span>
       </h2>
@@ -12,6 +16,7 @@ const FaqSection = () => {
           <p>Lorem ipsum dolor sit amet.</p>
           <p>Lorem ipsum dolor sit amet.</p>
         </div>
+        <div className="faq-line"></div>
       </div>
       <div className="question">
         <h4>Daily schedule? </h4>
@@ -19,6 +24,7 @@ const FaqSection = () => {
           <p>Lorem ipsum dolor sit amet.</p>
           <p>Lorem ipsum dolor sit amet.</p>
         </div>
+        <div className="faq-line"></div>
       </div>
       <div className="question">
         <h4>Different payment methods? </h4>
@@ -26,6 +32,7 @@ const FaqSection = () => {
           <p>Lorem ipsum dolor sit amet.</p>
           <p>Lorem ipsum dolor sit amet.</p>
         </div>
+        <div className="faq-line"></div>
       </div>
       <div className="question">
         <h4>What products to you offer? </h4>
@@ -33,9 +40,42 @@ const FaqSection = () => {
           <p>Lorem ipsum dolor sit amet.</p>
           <p>Lorem ipsum dolor sit amet.</p>
         </div>
+        <div className="faq-line"></div>
       </div>
-    </div>
+    </Faq>
   );
 };
+
+const Faq = styled(About)`
+  display: block;
+
+  span {
+    display: block;
+  }
+
+  h2 {
+    padding-bottom: 2rem;
+    font-weight: lighter;
+  }
+
+  .faq-line {
+    background: #cccccc;
+    height: 0.2rem;
+    margin: 2rem 0rem;
+    width: 100%;
+  }
+
+  .question {
+    padding: 3rem 0rem;
+    cursor: pointer;
+  }
+
+  .answer {
+    padding: 2rem 0rem;
+    p {
+      padding: 1rem 0rem;
+    }
+  }
+`;
 
 export default FaqSection;
