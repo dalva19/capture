8409d3e819ae198eda96a2ea156216de3c0977b2
dalva19 <75229602+dalva19@users.noreply.github.com>
2021-06-05 15:36:54 -0400

    added react router and links

--- src/App.js
+++ src/App.js
@@ -2,15 +2,29 @@ import React from "react";
 //global style
 import GlobalStyle from "./components/GlobalStyle";
 //import pages
-import AboutUs from "./pages/AboutUs";
 import Nav from "./components/Nav";
+import AboutUs from "./pages/AboutUs";
+import OurWork from "./pages/OurWork";
+import ContactUs from "./pages/ContactUs";
+//import Router
+import { Route, Switch } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
       <GlobalStyle />
       <Nav />
-      <AboutUs />
+      <Switch>
+        <Route exact path="/">
+          <AboutUs />
+        </Route>
+        <Route path="/work">
+          <OurWork />
+        </Route>
+        <Route path="/contact">
+          <ContactUs />
+        </Route>
+      </Switch>
     </div>
   );
 }
